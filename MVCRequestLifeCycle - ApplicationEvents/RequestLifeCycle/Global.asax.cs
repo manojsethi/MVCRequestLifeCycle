1b487427de312d9e6ca7ca2bc5da7014531799f3
Manoj Sethi <manojsethi@live.in>
2016-09-19 12:44:09 +0530

    Application Life Cylce Events

---
+++ MVCRequestLifeCycle - ApplicationEvents/RequestLifeCycle/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RequestLifeCycle
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_BeginRequest()
+        {
+            Debug.WriteLine("Begin Request");
+        }
+
+        protected void Application_MapRequestHandler()
+        {
+            Debug.WriteLine("Map Handler");
+        }
+
+        protected void Application_PostMapRequestHandler()
+        {
+            Debug.WriteLine("Post Map Handler");
+        }
+
+        protected void Application_AcquireRequestState()
+        {
+            Debug.WriteLine("Request State");
+        }
+
+        protected void Application_PreRequestHandlerExecute()
+        {
+            Debug.WriteLine("Pre Handler Execute");
+        }
+
+        protected void Application_PostRequestHandlerExecute()
+        {
+            Debug.WriteLine("Post Handler Execute");
+        }
+
+        protected void Application_EndRequest()
+        {
+            Debug.WriteLine("End Request");
+        }
+    }
+}
